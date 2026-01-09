6e5e910635465a80b449d57e266262f19e4c716d
abhays07 <itsrajputabhaysingh997@gmail.com>
2026-01-09 19:00:56 +0530

    Switch to BrowserRouter for clean URLs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, useLocation, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, useLocation, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { AnimatePresence, motion } from 'framer-motion';
 
